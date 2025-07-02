0c2b9c6d88107a8bb83d9cf4ba0d85edf4fbaaec
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-07-02 16:40:33 +0900

    change function

--- book.js
+++ book.js
@@ -19,16 +19,16 @@ function addBookToLibrary(objTitle, objAuthor, objPages, objRead) {
 
 addBookToLibrary("Harry Potter", "J.K. Rolling", "45", "read");
 addBookToLibrary("The art of Love", "Erich From", "43", "read");
-addBookToLibrary("What I talk about when I talk about running", "Harukami", "45", "read")
+addBookToLibrary("The Hobbit", "Harukami", "45", "read")
 
 const container = document.querySelector(".container");
 
-const card1 = document.createElement("div");
-const card2 = document.createElement("div");
-card1.classList.add("card1");
-card2.classList.add("card2");
+// const card1 = document.createElement("div");
+// const card2 = document.createElement("div");
+// card1.classList.add("card1");
+// card2.classList.add("card2");
 
-container.appendChild(card1);
+// container.appendChild(card1);
 // container.appendChild(card2);
 
 // console.log(container);
@@ -37,19 +37,19 @@ container.appendChild(card1);
 // card1.textContent = myLibrary[0].info();
 // div2.textContent = myLibrary[1].info();
 
-const title = document.createElement("p");
-card1.appendChild(title);
-const author = document.createElement("p");
-card1.appendChild(author);
-const pages = document.createElement("p");
-card1.appendChild(pages);
-const read = document.createElement("p");
-card1.appendChild(read);
+// const title = document.createElement("p");
+// card1.appendChild(title);
+// const author = document.createElement("p");
+// card1.appendChild(author);
+// const pages = document.createElement("p");
+// card1.appendChild(pages);
+// const read = document.createElement("p");
+// card1.appendChild(read);
 
-title.textContent = myLibrary[0].title;
-author.textContent = "by "+ myLibrary[0].author;
-pages.textContent = myLibrary[0].pages;
-read.textContent = myLibrary[0].read;
+// title.textContent = myLibrary[0].title;
+// author.textContent = "by "+ myLibrary[0].author;
+// pages.textContent = myLibrary[0].pages;
+// read.textContent = myLibrary[0].read;
 
 // loop has to make div and tags where the contents can place. 
 // then make a classlist with the 'i' so that I can get an access 
@@ -64,6 +64,7 @@ function makeCard() {
             this["author"+i] = document.createElement("p");
             this["pages"+i] = document.createElement("p");
             this["read"+i] = document.createElement("p");
+            this["card"+i].setAttribute("data-id", myLibrary[i].id);
 
             container.appendChild(this["card"+i]);
             this["card"+i].appendChild(this["title"+i]);
@@ -85,6 +86,7 @@ function makeCard() {
             this["author"+i] = document.createElement("p");
             this["pages"+i] = document.createElement("p");
             this["read"+i] = document.createElement("p");
+            this["card"+i].setAttribute("data-id", myLibrary[i].id);
 
             container.appendChild(this["card"+i]);
             this["card"+i].appendChild(this["title"+i]);
@@ -99,7 +101,6 @@ function makeCard() {
             this["card"+i].setAttribute("style", "background-color: red; display: flex; flex-direction: column; align-items: center; border-radius: 15px; border: 2px solid black; box-sizing: border-box;")
         }
     }
-
     
 };
 
@@ -141,3 +142,8 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     dialog.close();
 })
+
+// add a button on card display that can remove the display.
+// to remove it we will use event listenter click with remove child
+// what we need to do is how to make a button inside the card
+
