b3e1cd4864d3f1532e28ef8dfd802ee5a14b8100
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-07-03 11:32:35 +0900

    change makeCard function

--- book.js
+++ book.js
@@ -5,9 +5,6 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return (`${title} by ${author}, ${pages} pages, ${read}`)
-    }; 
 }
 
 function addBookToLibrary(objTitle, objAuthor, objPages, objRead) {
@@ -23,85 +20,49 @@ addBookToLibrary("The Hobbit", "Harukami", "45", "read")
 
 const container = document.querySelector(".container");
 
-// const card1 = document.createElement("div");
-// const card2 = document.createElement("div");
-// card1.classList.add("card1");
-// card2.classList.add("card2");
-
-// container.appendChild(card1);
-// container.appendChild(card2);
-
-// console.log(container);
-// comes null because it looks for element before the html fully parsed
-
-// card1.textContent = myLibrary[0].info();
-// div2.textContent = myLibrary[1].info();
-
-// const title = document.createElement("p");
-// card1.appendChild(title);
-// const author = document.createElement("p");
-// card1.appendChild(author);
-// const pages = document.createElement("p");
-// card1.appendChild(pages);
-// const read = document.createElement("p");
-// card1.appendChild(read);
-
-// title.textContent = myLibrary[0].title;
-// author.textContent = "by "+ myLibrary[0].author;
-// pages.textContent = myLibrary[0].pages;
-// read.textContent = myLibrary[0].read;
-
-// loop has to make div and tags where the contents can place. 
-// then make a classlist with the 'i' so that I can get an access 
-// to style them
-
 let first = true;
 function makeCard() {
-    if (first == true) {
-        for(let i = 0; i < myLibrary.length; i++) {
-            this["card"+i] = document.createElement("div");
-            this["title"+i] = document.createElement("p");
-            this["author"+i] = document.createElement("p");
-            this["pages"+i] = document.createElement("p");
-            this["read"+i] = document.createElement("p");
-            this["card"+i].setAttribute(`data-id`, myLibrary[i].id);
+    if ( first == true) {
+        myLibrary.forEach(function (book) {
+            const card = document.createElement("div");
+            container.appendChild(card);
+            const title = document.createElement("p");
+            const author = document.createElement("p");
+            const pages = document.createElement("p");
+            const read = document.createElement("p");
 
-            container.appendChild(this["card"+i]);
-            this["card"+i].appendChild(this["title"+i]);
-            this["card"+i].appendChild(this["author"+i]);
-            this["card"+i].appendChild(this["pages"+i]);
-            this["card"+i].appendChild(this["read"+i]);
+            card.appendChild(title);
+            card.appendChild(author);
+            card.appendChild(pages);
+            card.appendChild(read);
 
-            this["title"+i].textContent = myLibrary[i].title;
-            this["author"+i].textContent = "by "+ myLibrary[i].author;
-            this["pages"+i].textContent = myLibrary[i].pages+"pages";
-            this["read"+i].textContent = myLibrary[i].read;
-            this["card"+i].setAttribute("style", "background-color: red; display: flex; flex-direction: column; align-items: center; border-radius: 15px; border: 2px solid black; box-sizing: border-box;")
-        }
+            title.textContent = book.title;
+            author.textContent = book.author;
+            pages.textContent = book.pages;
+            read.textContent = book.read;
+        });
         first = false;
     } else if (first == false) {
-        for(let i = myLibrary.length-1; i < myLibrary.length; i++) {
-            this["card"+i] = document.createElement("div");
-            this["title"+i] = document.createElement("p");
-            this["author"+i] = document.createElement("p");
-            this["pages"+i] = document.createElement("p");
-            this["read"+i] = document.createElement("p");
-            this["card"+i].setAttribute(`data-id`, myLibrary[i].id);
+        for (let i = myLibrary.length-1; i < myLibrary.length; i++) {
+            const card = document.createElement("div");
+            container.appendChild(card);
+            const title = document.createElement("p");
+            const author = document.createElement("p");
+            const pages = document.createElement("p");
+            const read = document.createElement("p");
 
-            container.appendChild(this["card"+i]);
-            this["card"+i].appendChild(this["title"+i]);
-            this["card"+i].appendChild(this["author"+i]);
-            this["card"+i].appendChild(this["pages"+i]);
-            this["card"+i].appendChild(this["read"+i]);
+            card.appendChild(title);
+            card.appendChild(author);
+            card.appendChild(pages);
+            card.appendChild(read);
 
-            this["title"+i].textContent = myLibrary[i].title;
-            this["author"+i].textContent = "by "+ myLibrary[i].author;
-            this["pages"+i].textContent = myLibrary[i].pages+"pages";
-            this["read"+i].textContent = myLibrary[i].read;
-            this["card"+i].setAttribute("style", "background-color: red; display: flex; flex-direction: column; align-items: center; border-radius: 15px; border: 2px solid black; box-sizing: border-box;")
-        }
+            title.textContent = myLibrary[i].title;
+            author.textContent = myLibrary[i].author;
+            pages.textContent = myLibrary[i].pages;
+            read.textContent = myLibrary[i].read;            
+        } 
     }
-    
+
 };
 
 makeCard();
@@ -149,11 +110,3 @@ closeButton.addEventListener("click", () => {
 
 const cards = document.querySelectorAll("[data-id]");
 
-cards.forEach((card) =>  {
-    const rmbutton = document.createElement("button");
-    card.appendChild(rmbutton);
-    rmbutton.textContent = "Remove";
-    rmbutton.addEventListener("click", () => {
-        console.log("Remove button clicked")
-    })
-})
