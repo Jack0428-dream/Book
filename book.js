1b593a2c0d9b3074e744d39cf49f1d124c86e122
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-07-04 15:06:53 +0900

    change makeCard and addCard

--- book.js
+++ book.js
@@ -5,11 +5,11 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 }
 
 function addBookToLibrary(objTitle, objAuthor, objPages, objRead) {
     const newBook = new Book(objTitle, objAuthor, objPages, objRead);
-    newBook.id = crypto.randomUUID();
     myLibrary.push(newBook);
 }
 
@@ -20,73 +20,27 @@ addBookToLibrary("The Hobbit", "Harukami", "45", "read")
 
 const container = document.querySelector(".container");
 
-let first = true;
 function makeCard() {
-    if ( firs == true) {
-        myLibrary.forEach(function (book) {
-                const card = document.createElement("div");
-                card.classList.add("card");
-                container.appendChild(card);
-                const title = document.createElement("p");
-                const author = document.createElement("p");
-                const pages = document.createElement("p");
-                const read = document.createElement("p");
-                const rmbutton = document.createElement("button");
-                rmbutton.classList.add("remove");
+    container.innerHTML = "";
 
-                card.appendChild(title);
-                card.appendChild(author);
-                card.appendChild(pages);
-                card.appendChild(read);
-                card.appendChild(rmbutton);
+    myLibrary.forEach(function(book) {
+        const card = document.createElement("div");
+        const title = document.createElement("p");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const read = document.createElement("p");
 
-                title.textContent = book.title;
-                author.textContent = book.author;
-                pages.textContent = book.pages;
-                read.textContent = book.read;
-                rmbutton.textContent = "Remove"
-                card.setAttribute("data-id",book.id);
+        container.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
 
-                rmbutton.addEventListener("click", () => {
-                    console.log("button clicked")
-                    myLibrary.splice(myLibrary.findIndex(item => item.id == book.id),1);
-                    makeCard();
-                })
-            });
-            first = false;
-    } else if (first == false) {
-        for(let i = myLibrary.length-1; i < myLibrary.length; i++) {
-                const card = document.createElement("div");
-                card.classList.add("card");
-                container.appendChild(card);
-                const title = document.createElement("p");
-                const author = document.createElement("p");
-                const pages = document.createElement("p");
-                const read = document.createElement("p");
-                const rmbutton = document.createElement("button");
-                rmbutton.classList.add("remove");
-
-                card.appendChild(title);
-                card.appendChild(author);
-                card.appendChild(pages);
-                card.appendChild(read);
-                card.appendChild(rmbutton);
-
-                title.textContent = myLibrary[i].title;
-                author.textContent = myLibrary[i].author;
-                pages.textContent = myLibrary[i].pages;
-                read.textContent = myLibrary[i].read;
-                rmbutton.textContent = "Remove"
-                card.setAttribute("data-id",myLibrary[i].id);
-
-                rmbutton.addEventListener("click", () => {
-                    console.log("button clicked")
-                    myLibrary.splice(myLibrary.findIndex(item => item.id == myLibrary[i].id),1);
-                    makeCard();
-                })
-        }
-    }
-    
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        read.textContent = book.read;
+    })
 };
 
 makeCard();
@@ -98,25 +52,19 @@ const readin = document.querySelector("#read");
 const submit = document.querySelector("#submit")
 
 function addCard() {
-    let readval = "";
+    let addread = "";
     if (readin.checked == true) {
-        readval = "read"
+        addread = "read" 
     } else {
-        readval = "not read yet"
-    }
-    let tvalue = titlein.value;
-    let avalue = authorin.value;
-    let pvalue = parseInt(pagesin.value);
-    addBookToLibrary(tvalue, avalue, pvalue, readval);
-    makeCard();
-    titlein.value = "";
-    authorin.value = "";
-    pagesin.value = "";
-    readin.checked = false;
-};
+        addread = "Not yet"
+    }; 
+
+    addBookToLibrary(titlein.value, authorin.value, parseInt(pagesin.value), addread);
+}
 
 submit.addEventListener("click", () => {
     addCard();
+    makeCard();
 });
 
 
