6307ad86e998e464e34360a1af823af46ad3e9db
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-07-04 14:11:39 +0900

    rmbutton remake

--- book.js
+++ book.js
@@ -22,66 +22,71 @@ const container = document.querySelector(".container");
 
 let first = true;
 function makeCard() {
-    if ( first == true) {
+    if ( firs == true) {
         myLibrary.forEach(function (book) {
-            const card = document.createElement("div");
-            container.appendChild(card);
-            const title = document.createElement("p");
-            const author = document.createElement("p");
-            const pages = document.createElement("p");
-            const read = document.createElement("p");
-            const rmbutton = document.createElement("button");
+                const card = document.createElement("div");
+                card.classList.add("card");
+                container.appendChild(card);
+                const title = document.createElement("p");
+                const author = document.createElement("p");
+                const pages = document.createElement("p");
+                const read = document.createElement("p");
+                const rmbutton = document.createElement("button");
+                rmbutton.classList.add("remove");
 
-            card.appendChild(title);
-            card.appendChild(author);
-            card.appendChild(pages);
-            card.appendChild(read);
-            card.appendChild(rmbutton);
+                card.appendChild(title);
+                card.appendChild(author);
+                card.appendChild(pages);
+                card.appendChild(read);
+                card.appendChild(rmbutton);
 
-            title.textContent = book.title;
-            author.textContent = book.author;
-            pages.textContent = book.pages;
-            read.textContent = book.read;
-            rmbutton.textContent = "Remove"
-            card.setAttribute("data-id",book.id);
+                title.textContent = book.title;
+                author.textContent = book.author;
+                pages.textContent = book.pages;
+                read.textContent = book.read;
+                rmbutton.textContent = "Remove"
+                card.setAttribute("data-id",book.id);
 
-            rmbutton.addEventListener("click", () => {
-                if(book.id == card.dataset.id) {
-                    container.removeChild(card);
-                }
-            })
-        });
-        first = false;
+                rmbutton.addEventListener("click", () => {
+                    console.log("button clicked")
+                    myLibrary.splice(myLibrary.findIndex(item => item.id == book.id),1);
+                    makeCard();
+                })
+            });
+            first = false;
     } else if (first == false) {
-        for (let i = myLibrary.length-1; i < myLibrary.length; i++) {
-            const card = document.createElement("div");
-            container.appendChild(card);
-            const title = document.createElement("p");
-            const author = document.createElement("p");
-            const pages = document.createElement("p");
-            const read = document.createElement("p");
-            const rmbutton = document.createElement("button");
+        for(let i = myLibrary.length-1; i < myLibrary.length; i++) {
+                const card = document.createElement("div");
+                card.classList.add("card");
+                container.appendChild(card);
+                const title = document.createElement("p");
+                const author = document.createElement("p");
+                const pages = document.createElement("p");
+                const read = document.createElement("p");
+                const rmbutton = document.createElement("button");
+                rmbutton.classList.add("remove");
 
-            card.appendChild(title);
-            card.appendChild(author);
-            card.appendChild(pages);
-            card.appendChild(read);
-            card.appendChild(rmbutton);
+                card.appendChild(title);
+                card.appendChild(author);
+                card.appendChild(pages);
+                card.appendChild(read);
+                card.appendChild(rmbutton);
 
-            title.textContent = myLibrary[i].title;
-            author.textContent = myLibrary[i].author;
-            pages.textContent = myLibrary[i].pages;
-            read.textContent = myLibrary[i].read;
-            rmbutton.textContent = "Remove";
-            card.setAttribute("data-id",myLibrary[i].id);
+                title.textContent = myLibrary[i].title;
+                author.textContent = myLibrary[i].author;
+                pages.textContent = myLibrary[i].pages;
+                read.textContent = myLibrary[i].read;
+                rmbutton.textContent = "Remove"
+                card.setAttribute("data-id",myLibrary[i].id);
 
-            rmbutton.addEventListener("click", () => {
-                if(myLibrary[i].id == card.dataset.id) {
-                    container.removeChild(card);
-                }                
-            })
-        } 
+                rmbutton.addEventListener("click", () => {
+                    console.log("button clicked")
+                    myLibrary.splice(myLibrary.findIndex(item => item.id == myLibrary[i].id),1);
+                    makeCard();
+                })
+        }
     }
+    
 };
 
 makeCard();
@@ -92,7 +97,7 @@ const pagesin = document.querySelector("#pages");
 const readin = document.querySelector("#read");
 const submit = document.querySelector("#submit")
 
-function addCard(event) {
+function addCard() {
     let readval = "";
     if (readin.checked == true) {
         readval = "read"
@@ -108,7 +113,8 @@ function addCard(event) {
     authorin.value = "";
     pagesin.value = "";
     readin.checked = false;
-}
+};
+
 submit.addEventListener("click", () => {
     addCard();
 });
@@ -116,7 +122,7 @@ submit.addEventListener("click", () => {
 
 submit.addEventListener("click", () =>{
     dialog.close();
-})
+});
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
